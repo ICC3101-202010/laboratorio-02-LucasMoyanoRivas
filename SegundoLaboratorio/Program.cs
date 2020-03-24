60db00889c11ef9f8740b350f5d9432796d7a558
Lucas Moyano Rivas <lucasmoyanor@Lucass-MacBook-Pro-2.local>
2020-03-24 16:31:31 -0300

    Repositorio listo para comenzar a trabajar

---
+++ SegundoLaboratorio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SegundoLaboratorio
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
